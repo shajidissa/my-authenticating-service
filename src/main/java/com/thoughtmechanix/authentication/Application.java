0786d0f0cae52cda7fdb6603a761d9d3b933097c
Shajid Issa <shajidissa@Shajids-MacBook-Pro.local>
2017-12-22 02:30:04 +0000

    change to props

--- src/main/java/com/thoughtmechanix/authentication/Application.java
+++ src/main/java/com/thoughtmechanix/authentication/Application.java
@@ -2,6 +2,7 @@ package com.thoughtmechanix.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -16,6 +17,7 @@ import java.util.Map;
 @RestController
 @EnableResourceServer
 @EnableAuthorizationServer
+@EnableEurekaClient
 public class Application {
     @RequestMapping(value = { "/user" }, produces = "application/json")
     public Map<String, Object> user(OAuth2Authentication user) {
