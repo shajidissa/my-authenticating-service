4f33f4fe1b16e90ae1de3cb6e51291d94b2bb1a9
Shajid Issa <shajidissa@Shajids-MacBook-Pro.local>
2017-12-24 05:05:58 +0000

    adding auth server

--- src/main/java/com/thoughtmechanix/authentication/security/CorsConfig.java
+++ src/main/java/com/thoughtmechanix/authentication/security/CorsConfig.java
@@ -26,7 +26,6 @@ public FilterRegistrationBean corsFilter() {
     configAutenticacao.addAllowedHeader("Authorization");
     configAutenticacao.addAllowedHeader("Content-Type");
     configAutenticacao.addAllowedHeader("Accept");
-    //configAutenticacao.setAllowCredentials(true); 
     //configAutenticacao.addAllowedHeader("*");
     configAutenticacao.addAllowedMethod("POST");
     configAutenticacao.addAllowedMethod("GET");
@@ -34,8 +33,8 @@ public FilterRegistrationBean corsFilter() {
     configAutenticacao.addAllowedMethod("PUT");
     configAutenticacao.addAllowedMethod("OPTIONS");
     configAutenticacao.setMaxAge(3600L);
-    // source.registerCorsConfiguration("/oauth/token", configAutenticacao);
-    source.registerCorsConfiguration("/**", configAutenticacao); // Global for all paths
+    source.registerCorsConfiguration("/oauth/token", configAutenticacao);
+    // source.registerCorsConfiguration("/**", configAutenticacao); // Global for all paths
     
     FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
     bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
